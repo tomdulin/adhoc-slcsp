3031671e1756a4f1c7e797b1988a8e0c5a8383a6
tomdulin <tomldulin@gmail.com>
2023-10-17 12:27:29 -0700

    remove extra folder

--- slcsp/lib/slcsp/slcsp_data.rb
+++
@@ -1,7 +0,0 @@
-class SlcspData < Storable
-  private
-
-  def load_and_index
-    Reader.new(file_path).read.each{|args| @data[args["zipcode"]] << '' }
-  end
-end
--- slcsp/lib/slcsp/version.rb
+++
@@ -1,4 +0,0 @@
-module Slcsp
-  VERSION = '0.1.0'
-end
-
